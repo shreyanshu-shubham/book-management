fb99df4ca9b4d6c6b94f9f0e667dfe874e265a79
shreyanshu-shubham <84590909+shreyanshu-shubham@users.noreply.github.com>
2025-10-01 06:30:08 +0530

    basic db connection and crud

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,13 +1,6 @@
-create table authors (
-    id integer primary key,
-    first_name text not null,
-    last_name text
-);
-
 create table books (
     isbn bigint primary key,
     title text not null,
-    author_id int not null,
-    is_deleted boolean not null,
-    constraint fk_author foreign key (author_id) references authors (id)
+    author text not null,
+    is_deleted boolean not null
 );
---
+++ backend/models/book.go
@@ -0,0 +1,8 @@
+package models
+
+type Book struct {
+	Title string
+	ISBN_number int64
+	Author string
+	IsDeleted bool
+}
