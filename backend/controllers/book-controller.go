98ce21d38ccecc55325e97f7e05276bf6d58a9eb
shreyanshu-shubham <84590909+shreyanshu-shubham@users.noreply.github.com>
2025-10-01 08:41:54 +0530

    get all book by trash status completed

--- backend/controllers/book-controller.go
+++ backend/controllers/book-controller.go
@@ -12,7 +12,18 @@ import (
 )
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
-	data := db_utils.GetAllBooks()
+	include_trash, _ := strconv.ParseBool(r.URL.Query().Get("include_trash"))
+	only_trash, _ := strconv.ParseBool(r.URL.Query().Get("only_trash"))
+
+	var data []models.Book
+	if include_trash {
+		data = db_utils.GetAllBooks()
+	} else if only_trash {
+		data = db_utils.GetOnlyTrashedBooks()
+	} else {
+		data = db_utils.GetActiveBooks()
+	}
+
 	res, _ := json.Marshal(data)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -37,7 +48,7 @@ func TrashBook(w http.ResponseWriter, r *http.Request) {
 	}
 	db_utils.TrashBook(ISBN)
 }
-func RestoreBook(w http.ResponseWriter, r *http.Request)  {
+func RestoreBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	bookId := vars["isbn"]
 	ISBN, err := strconv.ParseInt(bookId, 0, 0)
