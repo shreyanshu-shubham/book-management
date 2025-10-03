7b0abfc502081763fa7c803131bc2f24f74cc8a9
shreyanshu-shubham <84590909+shreyanshu-shubham@users.noreply.github.com>
2025-10-04 02:36:06 +0530

    make build,docker,clean completed

--- backend/go.mod
+++ backend/go.mod
@@ -4,4 +4,8 @@ go 1.25.1
 
 require github.com/lib/pq v1.10.9
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -5,14 +5,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/shreyanshu-shubham/book-management/backend/routes"
 )
 
 func main() {
 	fmt.Println("Started....")
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With","Content-Type","Authorization"})
+	originOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+
 	r := mux.NewRouter()
 	routes.RegisterBookManagementRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9999", r))
+	log.Fatal(http.ListenAndServe("localhost:9999", handlers.CORS(headersOk, originOk,methodsOk)(r)))
 }
