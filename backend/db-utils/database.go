920140c068d9436130787bfa08911fc69e91d450
shreyanshu-shubham <84590909+shreyanshu-shubham@users.noreply.github.com>
2025-11-04 06:49:03 +0530

    logging changes

--- backend/db-utils/database.go
+++ backend/db-utils/database.go
@@ -12,9 +12,11 @@ func getConnection() *sql.DB {
 	connectionString := "postgres://postgres:postgres@localhost:5432/bkmanage?sslmode=disable"
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
+		log.Fatal("Failed to connect to DB")
 		log.Fatal(err)
 	}
 	if err = db.Ping(); err != nil {
+		log.Fatal("Ping failed to DB")
 		log.Fatal(err)
 	}
 	return db
@@ -24,8 +26,11 @@ func AddBook(book models.Book) bool {
 	db := getConnection()
 	defer db.Close()
 
+	log.Printf("inserting book: %+v\n",book)
+
 	_, st := GetBookByISBN(book.ISBN)
 	if st {
+		log.Fatal("Duplice ISBN error")
 		return false
 	}
 
@@ -33,10 +38,12 @@ func AddBook(book models.Book) bool {
 	query := `insert into books (isbn, title, author, is_deleted) values ($1,$2,$3,$4) returning isbn`
 	err := db.QueryRow(query, book.ISBN, book.Title, book.Author, book.IsDeleted).Scan(&isbn)
 	if err != nil {
+		log.Fatal("Failed to insert")
 		log.Fatal(err)
 		return false
 	}
 
+	log.Printf("inserted successfully")
 	return true
 }
 
@@ -44,8 +51,11 @@ func TrashBook(isbn int64) bool {
 	db := getConnection()
 	defer db.Close()
 
+	log.Printf("moving %d to trash",isbn)
+
 	_,st := GetBookByISBN(isbn)
 	if !st {
+		log.Fatal("book with isbn(",isbn,") does not exist")
 		return false
 	}
 
@@ -53,15 +63,23 @@ func TrashBook(isbn int64) bool {
 		"update books set is_deleted=true where isbn=$1",
 		isbn,
 	)
-	return err == nil
+	if err != nil {
+		log.Fatal("failed to trash book(isbn=",isbn,")")
+		return false
+	}
+
+	return true
 }
 
 func RestoreBook(isbn int64) bool {
 	db := getConnection()
 	defer db.Close()
 
+	log.Printf("restoring book(isbn=%d)",isbn)
+
 	_,st := GetBookByISBN(isbn)
 	if !st {
+		log.Fatalf("book (isbn=%d) does not exist",isbn)
 		return false
 	}
 
@@ -69,7 +87,13 @@ func RestoreBook(isbn int64) bool {
 		"update books set is_deleted=false where isbn=$1",
 		isbn,
 	)
-	return err == nil
+
+	if err != nil {
+		log.Fatalf("failed to restore book(isbn=%d)",isbn)
+		return false
+	}
+
+	return true
 }
 
 func DeleteBook(isbn int64) bool {
@@ -80,7 +104,12 @@ func DeleteBook(isbn int64) bool {
 		"delete from books where isbn=$1 and is_deleted=true",
 		isbn,
 	)
-	return err==nil
+
+	if err != nil {
+		log.Fatalf("failed to delete book(isbn=%d)",isbn)
+		return false
+	}
+	return true
 }
 
 func GetAllBooks() []models.Book {
@@ -90,6 +119,7 @@ func GetAllBooks() []models.Book {
 	data := []models.Book{}
 	rows, err := db.Query("select isbn,title,author,is_deleted from books")
 	if err != nil {
+		log.Fatalf("failed to get all book from DB")
 		log.Fatal(err)
 	}
 	defer rows.Close()
@@ -102,21 +132,24 @@ func GetAllBooks() []models.Book {
 	for rows.Next() {
 		err := rows.Scan(&isbn, &title, &author, &is_deleted)
 		if err != nil {
+			log.Fatalf("failed to scan row")
 			log.Fatal(err)
 		}
 		data = append(data, models.Book{Title: title, ISBN: isbn, Author: author, IsDeleted: is_deleted})
 	}
 
+	log.Printf("got %d books from DB",len(data))
 	return data
 }
 
 func GetOnlyTrashedBooks() []models.Book {
-	db := getConnection()
+	db := getConnection() 
 	defer db.Close()
 
 	data := []models.Book{}
 	rows, err := db.Query("select isbn,title,author,is_deleted from books where is_deleted=true")
 	if err != nil {
+		log.Fatalf("failed to get trashed book list from DB")
 		log.Fatal(err)
 	}
 	defer rows.Close()
@@ -129,11 +162,13 @@ func GetOnlyTrashedBooks() []models.Book {
 	for rows.Next() {
 		err := rows.Scan(&isbn, &title, &author, &is_deleted)
 		if err != nil {
+			log.Fatal("failed to scan row")
 			log.Fatal(err)
 		}
 		data = append(data, models.Book{Title: title, ISBN: isbn, Author: author, IsDeleted: is_deleted})
 	}
 
+	log.Printf("fetched total of %d trashed books",len(data))
 	return data
 }
 
@@ -144,6 +179,7 @@ func GetActiveBooks() []models.Book {
 	data := []models.Book{}
 	rows, err := db.Query("select isbn,title,author,is_deleted from books where is_deleted=false")
 	if err != nil {
+		log.Fatal("failed to get books from DB")
 		log.Fatal(err)
 	}
 	defer rows.Close()
@@ -156,11 +192,13 @@ func GetActiveBooks() []models.Book {
 	for rows.Next() {
 		err := rows.Scan(&isbn, &title, &author, &is_deleted)
 		if err != nil {
+			log.Fatal("failed to scan row")
 			log.Fatal(err)
 		}
 		data = append(data, models.Book{Title: title, ISBN: isbn, Author: author, IsDeleted: is_deleted})
 	}
 
+	log.Printf("fetched total of %d active books",len(data))
 	return data
 }
 
@@ -175,11 +213,16 @@ func GetBookByISBN(isbn int64) (models.Book, bool) {
 	err := db.QueryRow(query, isbn).Scan(&title, &author, &is_deleted)
 	if err != nil {
 		if err == sql.ErrNoRows {
+			log.Fatalf("no book with isbn=%d",isbn)
 			return models.Book{}, false
 		}
+		log.Fatalf("failed to fetch from DB")
 		log.Fatal(err)
 	}
-	return models.Book{Title: title, ISBN: isbn, Author: author, IsDeleted: is_deleted}, true
+
+	data := models.Book{Title: title, ISBN: isbn, Author: author, IsDeleted: is_deleted}
+	log.Printf("got book from DB: %+v",data)
+	return data, true
 }
 
 func RestoreTrash() bool {
@@ -189,7 +232,12 @@ func RestoreTrash() bool {
 	_, err := db.Exec(
 		"update books set is_deleted=false where is_deleted=true",
 	)
-	return err == nil
+
+	if err != nil {
+		log.Fatalf("failed to restore books from the trash in")
+		log.Fatal(err)
+	}
+	return true
 }
 
 func EmptyTrash() bool {
@@ -199,12 +247,18 @@ func EmptyTrash() bool {
 	_, err := db.Exec(
 		"delete from books where is_deleted=true",
 	)
-	return err == nil
+	if err != nil {
+		log.Fatalf("failed to delete books from the trash in DB")
+		log.Fatal(err)
+	}
+	return true
 }
 
 func UpdateBook(book models.Book) (bool) {
 	db := getConnection()
 	defer db.Close()
+
+	
 
 	_, st := GetBookByISBN(book.ISBN)
 	if !st {
